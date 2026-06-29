aec38728a94ffadf0610aa8bc22975b044eeccd3
wqwqwq999888-prog <wqwqwq999888@gmail.com>
2026-06-29 22:53:07 +0800

    Create ecpay-return.js

---
+++ netlify/functions/ecpay-return.js
@@ -0,0 +1,45 @@
+const crypto = require('crypto');
+
+exports.handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  const HASH_KEY = process.env.ECPAY_HASH_KEY;
+  const HASH_IV = process.env.ECPAY_HASH_IV;
+
+  const params = Object.fromEntries(new URLSearchParams(event.body));
+  const receivedMac = params.CheckMacValue;
+  delete params.CheckMacValue;
+
+  const sorted = Object.keys(params).sort((a, b) =>
+    a.toLowerCase().localeCompare(b.toLowerCase())
+  );
+  let str = `HashKey=${HASH_KEY}`;
+  sorted.forEach(key => { str += `&${key}=${params[key]}`; });
+  str += `&HashIV=${HASH_IV}`;
+  str = encodeURIComponent(str).toLowerCase();
+  str = str
+    .replace(/%2d/g, '-').replace(/%5f/g, '_')
+    .replace(/%2e/g, '.').replace(/%21/g, '!')
+    .replace(/%2a/g, '*').replace(/%28/g, '(')
+    .replace(/%29/g, ')');
+
+  const computedMac = crypto
+    .createHash('sha256').update(str).digest('hex').toUpperCase();
+
+  if (computedMac !== receivedMac) {
+    console.error('CheckMacValue 驗證失敗');
+    return { statusCode: 200, body: '0|Error' };
+  }
+
+  if (params.RtnCode === '1') {
+    console.log('付款成功，訂單編號:', params.MerchantTradeNo);
+  }
+
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'text/plain' },
+    body: '1|OK',
+  };
+};
